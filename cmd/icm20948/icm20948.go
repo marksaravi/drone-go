65322ef2ae59b8565380674e29803772f319c206
marksaravi <mark.saravi@gmail.com>
2020-11-03 19:36:35 +0000

    working SPI reader

--- cmd/icm20948/icm20948.go
+++ cmd/icm20948/icm20948.go
@@ -2,45 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	"periph.io/x/periph/conn/physic"
-	"periph.io/x/periph/conn/spi"
-	"periph.io/x/periph/host/sysfs"
+	"github.com/MarkSaravi/drone-go/devices/icm20948"
 )
 
 func main() {
-	// icm20948, err := icm20948.NewRaspberryPiDriver(0, 0)
-	// defer icm20948.Close()
+	// b, err := sysfs.NewSPI(0, 0)
 	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer b.Close()
+
+	// c, err := b.Connect(7*physic.MegaHertz, spi.Mode3, 8)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// r := make([]byte, 2)
+	// if err := c.Tx([]byte{0b10000000, 0x0}, r); err != nil {
 	// 	fmt.Println(err.Error())
 	// }
-	// fmt.Println(icm20948)
-	// tx := make([]byte, 2)
-	// tx[0] = 0
-	// tx[1] = 0
-	// rx := make([]byte, 2)
-	// err = icm20948.Connection.Tx([]byte{0x1, 0x0}, rx)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// } else {
-	// 	fmt.Println(rx)
-	// }
-	b, err := sysfs.NewSPI(0, 0)
+	// fmt.Printf("%X\n", r)
+	icm20948, err := icm20948.NewRaspberryPiICM20948Driver("/dev/spidev0.0")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err.Error())
+		return
 	}
-	defer b.Close()
-
-	c, err := b.Connect(7*physic.MegaHertz, spi.Mode3, 8)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	r := make([]byte, 2)
-	if err := c.Tx([]byte{0b10000000, 0x0}, r); err != nil {
+	if data, err := icm20948.Read(0x0); err == nil {
+		fmt.Println(data)
+	} else {
 		fmt.Println(err.Error())
 	}
-	fmt.Printf("%X\n", r)
+	defer icm20948.Close()
 }
--- devices/icm20948/icm20948.go
+++ devices/icm20948/icm20948.go
@@ -1,28 +1,48 @@
 package icm20948
 
 import (
-	"github.com/MarkSaravi/drone-go/drivers/spi"
-	xspi "periph.io/x/periph/conn/spi"
+	"periph.io/x/periph/conn/physic"
+	"periph.io/x/periph/conn/spi"
+	"periph.io/x/periph/conn/spi/spireg"
+	"periph.io/x/periph/host"
 )
 
+// Driver for ICM20948
 type Driver struct {
 	name string
-	spi.Connection
+	spi.PortCloser
+	spi.Conn
 }
 
-// NewDriver creates new ICM20948 driver
-func NewDriver(busNum int, chipNum int, mode xspi.Mode, maxSpeed int64, bits int) (*Driver, error) {
-	connection, err := spi.GetSpiConnection(busNum, chipNum, mode, maxSpeed, bits)
+func init() {
+	host.Init()
+}
+
+// NewRaspberryPiICM20948Driver creates ICM20948 driver for raspberry pi
+func NewRaspberryPiICM20948Driver(devname string) (*Driver, error) {
+	dev, err := spireg.Open(devname)
+	if err != nil {
+		return nil, err
+	}
+	connection, err := dev.Connect(7*physic.MegaHertz, spi.Mode3, 8)
 	if err != nil {
 		return nil, err
 	}
 	return &Driver{
 		name:       "ICM-20948",
-		Connection: connection,
+		PortCloser: dev,
+		Conn:       connection,
 	}, nil
 }
 
-// NewRaspberryPiDriver for raspberry pi
-func NewRaspberryPiDriver(busNum int, chipNum int) (*Driver, error) {
-	return NewDriver(busNum, chipNum, xspi.Mode3, 7000000, 8)
+//Close closes the device
+func (d *Driver) Close() {
+	d.PortCloser.Close()
+}
+
+// Read reads a byte from address
+func (d *Driver) Read(address byte) (byte, error) {
+	r := make([]byte, 2)
+	err := d.Conn.Tx([]byte{0b10000000 | address, 0x0}, r)
+	return r[1], err
 }
