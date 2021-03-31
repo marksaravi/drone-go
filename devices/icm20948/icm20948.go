435cddbf3ef2718e85ebdad29f9a15bf50bbfa38
marksaravi <mark.saravi@gmail.com>
2021-04-01 08:36:16 +1100

    refactoring code

--- devices/icm20948/icm20948.go
+++ devices/icm20948/icm20948.go
@@ -11,92 +11,11 @@ import (
 	"periph.io/x/periph/host/sysfs"
 )
 
-const (
-	BANK0 uint16 = 0 << 8
-	BANK1 uint16 = 1 << 8
-	BANK2 uint16 = 2 << 8
-	BANK3 uint16 = 3 << 8
-)
-
-const (
-	REG_BANK_SEL byte = 0x7F
-
-	// BANK0
-	WHO_AM_I     uint16 = BANK0 | 0x0
-	LP_CONFIG    uint16 = BANK0 | 0x5
-	PWR_MGMT_1   uint16 = BANK0 | 0x6
-	PWR_MGMT_2   uint16 = BANK0 | 0x7
-	INT_ENABLE_3 uint16 = BANK0 | 0x13
-	ACCEL_XOUT_H uint16 = BANK0 | 0x2D
-	ACCEL_XOUT_L uint16 = BANK0 | 0x2E
-	ACCEL_YOUT_H uint16 = BANK0 | 0x2F
-	ACCEL_YOUT_L uint16 = BANK0 | 0x30
-	ACCEL_ZOUT_H uint16 = BANK0 | 0x31
-	ACCEL_ZOUT_L uint16 = BANK0 | 0x32
-	GYRO_XOUT_H  uint16 = BANK0 | 0x33
-	GYRO_XOUT_L  uint16 = BANK0 | 0x34
-	GYRO_YOUT_H  uint16 = BANK0 | 0x35
-	GYRO_YOUT_L  uint16 = BANK0 | 0x36
-	GYRO_ZOUT_H  uint16 = BANK0 | 0x37
-	GYRO_ZOUT_L  uint16 = BANK0 | 0x38
-
-	// BANK1
-	XA_OFFS_H uint16 = BANK1 | 0x14
-
-	// BANK2
-	GYRO_SMPLRT_DIV uint16 = BANK2 | 0x0
-	GYRO_CONFIG_1   uint16 = BANK2 | 0x1
-	GYRO_CONFIG_2   uint16 = BANK2 | 0x2
-	ZG_OFFS_USRL    uint16 = BANK2 | 0x8
-	ACCEL_CONFIG    uint16 = BANK2 | 0x14
-	ACCEL_CONFIG_2  uint16 = BANK2 | 0x15
-	MOD_CTRL_USR    uint16 = BANK2 | 0x54
-)
-
 func reg(reg uint16) *Register {
 	return &Register{
 		address: byte(reg),
 		bank:    byte(reg >> 8),
 	}
-}
-
-// Register is the address and bank of the register
-type Register struct {
-	address byte
-	bank    byte
-}
-
-type threeAxis struct {
-	data     sensore.XYZ
-	prevData sensore.XYZ
-	dataDiff float64
-	config   sensore.Config
-}
-
-// DeviceConfig is the configuration for the device
-type DeviceConfig struct {
-}
-
-// AccelerometerConfig is the configurations for Accelerometer
-type AccelerometerConfig struct {
-	Sensitivity int
-}
-
-// GyroscopeConfig is the configuration for Gyroscope
-type GyroscopeConfig struct {
-	FullScale int
-}
-
-// Device is icm20948 mems
-type Device struct {
-	*sysfs.SPI
-	spi.Conn
-	regbank     byte
-	lastReading int64
-	duration    int64
-	config      DeviceConfig
-	acc         threeAxis
-	gyro        threeAxis
 }
 
 var accelerometerSensitivity = make(map[int]float64)
---
+++ devices/icm20948/types.go
@@ -0,0 +1,46 @@
+package icm20948
+
+import (
+	"github.com/MarkSaravi/drone-go/types/sensore"
+	"periph.io/x/periph/conn/spi"
+	"periph.io/x/periph/host/sysfs"
+)
+
+// Register is the address and bank of the register
+type Register struct {
+	address byte
+	bank    byte
+}
+
+type threeAxis struct {
+	data     sensore.XYZ
+	prevData sensore.XYZ
+	dataDiff float64
+	config   sensore.Config
+}
+
+// DeviceConfig is the configuration for the device
+type DeviceConfig struct {
+}
+
+// AccelerometerConfig is the configurations for Accelerometer
+type AccelerometerConfig struct {
+	Sensitivity int
+}
+
+// GyroscopeConfig is the configuration for Gyroscope
+type GyroscopeConfig struct {
+	FullScale int
+}
+
+// Device is icm20948 mems
+type Device struct {
+	*sysfs.SPI
+	spi.Conn
+	regbank     byte
+	lastReading int64
+	duration    int64
+	config      DeviceConfig
+	acc         threeAxis
+	gyro        threeAxis
+}
