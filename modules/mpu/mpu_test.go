b779d04399566ded7c858da4ac64d936326f5410
marksaravi <mark.saravi@gmail.com>
2021-04-02 17:42:45 +1100

    testing step 2

--- modules/mpu/mpu_test.go
+++ modules/mpu/mpu_test.go
@@ -19,17 +19,26 @@ func TestNew(t *testing.T) {
 
 func TestPushToFront(t *testing.T) {
 	data := mpu.New(4)
-	for i := 1; i <= 4; i++ {
-		data.PushToFront(types.XYZ{X: float64(i), Y: float64(i), Z: float64(i)})
+	want2 := []types.XYZ{
+		{X: 2, Y: 2, Z: 2},
+		{X: 1, Y: 1, Z: 1},
+		{X: 0, Y: 0, Z: 0},
+		{X: 0, Y: 0, Z: 0},
 	}
-	want := []types.XYZ{
+	want4 := []types.XYZ{
 		{X: 4, Y: 4, Z: 4},
 		{X: 3, Y: 3, Z: 3},
 		{X: 2, Y: 2, Z: 2},
 		{X: 1, Y: 1, Z: 1},
 	}
-	got := data.GetBuffer()
-	if !reflect.DeepEqual(want, got) {
-		t.Errorf("got %v, want %v", got, want)
+	data.PushToFront(types.XYZ{X: 1, Y: 1, Z: 1})
+	data.PushToFront(types.XYZ{X: 2, Y: 2, Z: 2})
+	if !reflect.DeepEqual(want2, data.GetBuffer()) {
+		t.Errorf("got %v, want %v", data.GetBuffer(), want4)
+	}
+	data.PushToFront(types.XYZ{X: 3, Y: 3, Z: 3})
+	data.PushToFront(types.XYZ{X: 4, Y: 4, Z: 4})
+	if !reflect.DeepEqual(want4, data.GetBuffer()) {
+		t.Errorf("got %v, want %v", data.GetBuffer(), want4)
 	}
 }
