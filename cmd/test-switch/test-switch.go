384c2652479c93733b0bc1362681097c74fa067f
marksaravi <mark.saravi@gmail.com>
2021-08-21 12:57:07 +1000

    pull up mode

--- cmd/test-switch/test-switch.go
+++ cmd/test-switch/test-switch.go
@@ -15,18 +15,18 @@ func main() {
 		log.Fatal(err)
 	}
 	var pin gpio.PinIn = gpioreg.ByName("GPIO26")
-	pin.In(gpio.PullDown, gpio.NoEdge)
+	pin.In(gpio.PullUp, gpio.NoEdge)
 	if pin == nil {
 		log.Fatal("Failed to find ")
 	}
-	var level gpio.Level = gpio.Low
+	var level gpio.Level = gpio.High
 	var counter int = 0
 	var ptime time.Time = time.Now()
 	for {
 		nl := pin.Read()
 		if nl != level {
 			level = nl
-			if level == gpio.High {
+			if level == gpio.Low {
 				t := time.Now()
 				dur := time.Since(ptime)
 				ptime = t
