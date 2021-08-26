d3e65117ff98c268396950c30b1ccd77f25949c1
marksaravi <mark.saravi@gmail.com>
2021-08-26 21:37:59 +1000

    increasing read sequence

--- cmd/remote/main.go
+++ cmd/remote/main.go
@@ -18,6 +18,6 @@ func main() {
 	for {
 		rd := remoteControl.ReadInputs()
 		fmt.Printf("X: %4.1f, Y: %4.1f, Z: %4.1f, T: %4.1f, Stop: %v\n", rd.X, rd.Y, rd.Z, rd.Throttle, rd.Stop)
-		time.Sleep(time.Millisecond * 100)
+		time.Sleep(time.Millisecond * 10)
 	}
 }
