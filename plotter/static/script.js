be6ba43d57319a403a87bcf6c838752042825352
Mark Saravi <mark.saravi@gmail.com>
2021-09-12 16:38:55 +1000

    still trying

--- plotter/static/script.js
+++ plotter/static/script.js
@@ -1,7 +1,8 @@
-const CANVAS_TIME_SECONDS = 3
+const DATA_PER_SECOND = 3200
+const CANVAS_TIME_SECONDS = 10
 var TIME_SCALE = 1e9
+const MAX_BUFFER_SIZE = DATA_PER_SECOND * 60
 
-let startTime = 0
 let grpah = null;
 let canvasWidth = 0;
 let canvasHeight = 0;
@@ -10,6 +11,7 @@ var gyroCtx = null;
 var xScale = 1
 var yScale = 1
 var maxAngle = 90
+const xyBuffer = new Array(DATA_PER_SECOND * CANVAS_TIME_SECONDS)
 
 
 function setupCanvas() {
@@ -19,71 +21,74 @@ function setupCanvas() {
     gyroCanvas = document.getElementById("gyro");
     xScale = canvasWidth / CANVAS_TIME_SECONDS / TIME_SCALE
     yScale = canvasHeight / 2 / maxAngle
+    gyroCtx = gyroCanvas.getContext("2d");
 }
 
-function plot(buffer) {
-    const gyroCtx = gyroCanvas.getContext("2d");
-    gyroCtx.lineWidth = 1;
-    console.log('plot', buffer.length);
-    const lastTime = buffer[buffer.length - 1].t
-    const totalGraphTime = (lastTime - startTime)
-    const xEnd = totalGraphTime * xScale
-    gyroCtx.fillStyle = "red";
-    gyroCtx.fillRect(0, 0, 800, 100);
-    gyroCtx.stroke();
-    let xOffset = 0
-    if (xEnd > canvasWidth) {
-        xOffset = xEnd - canvasWidth
-    }
-    let first = true
-    let prevX = 0
-    gyroCtx.clearRect(0, 0, 800, 100)
-    gyroCtx.stroke();
-    for (let i = buffer.length - 1; i >= 0; i--) {
-        const d = buffer[i]
-        const x = (d.t - startTime) * xScale - xOffset
-        const y = canvasHeight / 2 - d.r.r * yScale
-        if (x<0) {
-            console.log(i)
-            break
+function plot(buffer, lastIndex, numOfPackets) {
+    let i = lastIndex;
+    let packetCounter = 0
+    let dataCounter = 0
+    while (packetCounter < numOfPackets && dataCounter < xyBuffer.length) {
+        i--;
+        if (i < 0) {
+            i = MAX_BUFFER_SIZE
         }
+        packetCounter++
+        xyBuffer[dataCounter] = {
+            rRol: buffer[i].r.r,
+            t: buffer[i].t
+        }
+        dataCounter++
+    }
+    gyroCtx.clearRect(0, 0, 800, 100);
+    gyroCtx.stroke();
+    let first = true;
+    const startTime = xyBuffer[dataCounter - 1].t;
+    let prevX = 0
+    let strokeCounter = 0
+    for (let i = 0; i < dataCounter; i++) {
+        const x = canvasWidth - (xyBuffer[i].t - startTime) * xScale
+        const y = canvasHeight / 2 - xyBuffer[i].rRol * yScale
         if (first) {
+            first = false;
+            strokeCounter++;
             gyroCtx.moveTo(x, y)
-            prevX = x;
-            first = false
+            prevX = x
         } else {
-            if (Math.floor(prevX)!==Math.floor(x)) {
-                prevX = x
+            if (Math.floor(prevX) !== Math.floor(x)) {
                 gyroCtx.lineTo(x, y)
+                prevX = x;
+                strokeCounter++;
             }
         }
     }
+    console.log(strokeCounter)
     gyroCtx.stroke();
-    
 }
 function createWebSocket() {
     // Create WebSocket connection.
     console.log("establishing connection");
     const socket = new WebSocket('ws://localhost:8081/conn');
 
-    window.plotterBuffer = []
+    window.plotterBuffer = new Array(MAX_BUFFER_SIZE)
+    window.plotterBufferLastIndex = 0
+    window.plotterBufferCounter = 0
     // Listen for messages
     socket.addEventListener('message', function (event) {
         const packets = JSON.parse(JSON.parse(event.data));
-        if (startTime === 0) {
-            startTime = packets[0].t
-        }
-        window.plotterBuffer = [...window.plotterBuffer, ...packets]
-        // packet.map(element => {
-        //     buffer.push(element)
-        // });
-        const maxlen = 5000
-        const l1 = window.plotterBuffer.length
-        if (window.plotterBuffer.length > maxlen) {
-            window.plotterBuffer = window.plotterBuffer.slice(window.plotterBuffer.length-maxlen)
-            console.log("slicing", window.plotterBuffer.length, l1)
-        }
-        plot(window.plotterBuffer)
+        packets.forEach(p => {
+            window.plotterBuffer[window.plotterBufferLastIndex] = p
+            window.plotterBufferLastIndex++
+            if (window.plotterBufferLastIndex == MAX_BUFFER_SIZE) {
+                window.plotterBufferLastIndex = 0
+            }
+
+            if (window.plotterBufferCounter < MAX_BUFFER_SIZE) {
+                window.plotterBufferCounter++
+            }
+        })
+        // console.log(window.plotterBufferLastIndex)
+        plot(window.plotterBuffer, window.plotterBufferLastIndex, window.plotterBufferCounter)
     });
 }
 
