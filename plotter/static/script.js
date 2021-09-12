dcee6565780022b8226ed58e0f3eadb0ee3df655
Mark Saravi <mark.saravi@gmail.com>
2021-09-13 07:45:22 +1000

    removing old contextes

--- plotter/static/script.js
+++ plotter/static/script.js
@@ -1,30 +1,44 @@
 const DATA_PER_SECOND = 3200
-const CANVAS_TIME_SECONDS = 1
+const CANVAS_TIME_SECONDS = 10
 var TIME_SCALE = 1e9
-const MAX_BUFFER_SIZE = DATA_PER_SECOND * 2
+const MAX_BUFFER_SIZE = DATA_PER_SECOND * 120
 
-let grpah = null;
+const graphSettings = {
+    width: 0,
+    height: 0,
+    xScale: 1,
+    yScale: 1,
+    timeSpan: 10,
+    yMax: 90,
+    axis: 'roll',
+}
+
+const ctx2D = {
+    accelerometer: null,
+    gyroscope: null,
+    rotations: null,
+}
+
 let canvasWidth = 0;
 let canvasHeight = 0;
-var gyroCanvas = null;
-var gyroCtx = null;
-var accCanvas = null;
-var accCtx = null;
-var rotCanvas = null;
-var rotCtx = null;
 var xScale = 1
 var yScale = 1
 var maxAngle = 90
 const xyBuffer = new Array(DATA_PER_SECOND * CANVAS_TIME_SECONDS)
 
+const grapgs = ['accelerometer', 'gyroscope', 'rotations']
 
 function setupContainer() {
-    const container = document.getElementById('canvas-container');
-    const acc = document.getElementById('acc');
-    canvasWidth = container.offsetWidth;
-    canvasHeight = acc.offsetHeight;
-    xScale = canvasWidth / CANVAS_TIME_SECONDS / TIME_SCALE;
-    yScale = canvasHeight / 2 / maxAngle;
+    const container = document.getElementById('canvas-container')
+    const acc = document.getElementById('accelerometer')
+    graphSettings.width = container.offsetWidth
+    graphSettings.height = container.offsetHeight
+    canvasWidth = container.offsetWidth
+    canvasHeight = acc.offsetHeight
+    xScale = canvasWidth / CANVAS_TIME_SECONDS / TIME_SCALE
+    yScale = canvasHeight / 2 / maxAngle
+    graphSettings.xScale = canvasWidth / CANVAS_TIME_SECONDS / TIME_SCALE
+    graphSettings.yScale = canvasHeight / 2 / maxAngle
 }
 
 function getCanvasContext(id) {
@@ -37,33 +51,34 @@ function getCanvasContext(id) {
 
 function setupPlotter() {
     setupContainer()
-    accCtx = getCanvasContext("acc");
-    gyroCtx = getCanvasContext("gyro");
-    rotCtx = getCanvasContext("rotations");
+    ctx2D.accelerometer = getCanvasContext("accelerometer");
+    ctx2D.gyroscope = getCanvasContext("gyroscope");
+    ctx2D.rotations = getCanvasContext("rotations");
+
+}
+
+function getContextes(action) {
+    grapgs.forEach(g => {
+        action(ctx2D[g])
+    })
 }
 
 function clearCanvases() {
-    accCtx.clearRect(0, 0, canvasWidth, canvasHeight);
-    gyroCtx.clearRect(0, 0, canvasWidth, canvasHeight);
-    rotCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+    getContextes((ctx)=> ctx.clearRect(0, 0, canvasWidth, canvasHeight));
 }
 
 function beginPath() {
-    accCtx.beginPath();
-    gyroCtx.beginPath();
-    rotCtx.beginPath();
+    getContextes((ctx)=> ctx.beginPath());
 }
 
 function lineTo(x, ay, gy, ry) {
-    accCtx.lineTo(x, ay)
-    gyroCtx.lineTo(x, gy)
-    rotCtx.lineTo(x, ry)
+    ctx2D['accelerometer'].lineTo(x, ay)
+    ctx2D['gyroscope'].lineTo(x, gy)
+    ctx2D['rotations'].lineTo(x, ry)
 }
 
 function stroke() {
-    accCtx.stroke();
-    gyroCtx.stroke();
-    rotCtx.stroke();
+    getContextes((ctx)=> ctx.stroke());
 }
 
 function plot(datalink) {
@@ -116,17 +131,17 @@ function createWebSocket() {
         }
         newlink.prev = link
         link.next = newlink
-        link=link.next
+        link = link.next
     }
-    link.next=firstlink
-    firstlink.prev=link
+    link.next = firstlink
+    firstlink.prev = link
 
     socket.addEventListener('message', function (event) {
         const packets = JSON.parse(JSON.parse(event.data));
         packets.forEach(p => {
-            link=link.next
-            link.data=p
-            link.next.data=null
+            link = link.next
+            link.data = p
+            link.next.data = null
         })
         plot(link)
     });
