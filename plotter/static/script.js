b49ed681a23b6b3e66fcb9d6ce7fded608b14a47
Mark Saravi <mark.saravi@gmail.com>
2021-09-12 17:19:44 +1000

    working plotter

--- plotter/static/script.js
+++ plotter/static/script.js
@@ -8,6 +8,10 @@ let canvasWidth = 0;
 let canvasHeight = 0;
 var gyroCanvas = null;
 var gyroCtx = null;
+var accCanvas = null;
+var accCtx = null;
+var rotCanvas = null;
+var rotCtx = null;
 var xScale = 1
 var yScale = 1
 var maxAngle = 90
@@ -18,10 +22,14 @@ function setupCanvas() {
     grpah = document.querySelector('#gyro');
     canvasWidth = grpah.offsetWidth;
     canvasHeight = grpah.offsetHeight;
+    accCanvas = document.getElementById("acc");
     gyroCanvas = document.getElementById("gyro");
+    rotCanvas = document.getElementById("rotations");
     xScale = canvasWidth / CANVAS_TIME_SECONDS / TIME_SCALE
     yScale = canvasHeight / 2 / maxAngle
+    accCtx = accCanvas.getContext("2d");
     gyroCtx = gyroCanvas.getContext("2d");
+    rotCtx = rotCanvas.getContext("2d");
 }
 
 function plot(buffer, lastIndex, numOfPackets) {
@@ -35,24 +43,36 @@ function plot(buffer, lastIndex, numOfPackets) {
         }
         packetCounter++
         xyBuffer[dataCounter] = {
+            aRol: buffer[i].a.r,
+            gRol: buffer[i].g.r,
             rRol: buffer[i].r.r,
             t: buffer[i].t
         }
         dataCounter++
     }
     const startTime = xyBuffer[dataCounter - 1].t;
+    accCtx.clearRect(0, 0, 800, 100);
     gyroCtx.clearRect(0, 0, 800, 100);
+    rotCtx.clearRect(0, 0, 800, 100);
+    accCtx.beginPath();
     gyroCtx.beginPath();
+    rotCtx.beginPath();
     let prevX = 0
     for (let i = dataCounter - 1; i >= 0; i--) {
         const x = canvasWidth - (xyBuffer[i].t - startTime) * xScale
-        const y = canvasHeight / 2 - xyBuffer[i].rRol * yScale
+        const ay = canvasHeight / 2 - xyBuffer[i].aRol * yScale
+        const gy = canvasHeight / 2 - xyBuffer[i].gRol * yScale
+        const ry = canvasHeight / 2 - xyBuffer[i].rRol * yScale
         if (Math.floor(prevX)!=Math.floor(x)) {
             prevX = x
-            gyroCtx.lineTo(x, y)
+            accCtx.lineTo(x, ay)
+            gyroCtx.lineTo(x, gy)
+            rotCtx.lineTo(x, ry)
         }
     }
+    accCtx.stroke();
     gyroCtx.stroke();
+    rotCtx.stroke();
 }
 function createWebSocket() {
     // Create WebSocket connection.
