525896c1fe1a6a20165f3c83ed1923be4bc6dd6b
marksaravi <mark.saravi@gmail.com>
2021-12-30 05:02:56 +0000

    updating print

--- apps/flightcontrol/flightcontrol.go
+++ apps/flightcontrol/flightcontrol.go
@@ -103,6 +103,6 @@ var lastShowFlightCommands time.Time
 func showFlightCommands(fc models.FlightCommands) {
 	if time.Since(lastShowFlightCommands) >= time.Second/2 {
 		lastShowFlightCommands = time.Now()
-		log.Printf("%8.2f, %8.2f, %8.2f, %8.2f, %t, %t, %t, %t, %t, %t", fc.Roll, fc.Pitch, fc.Yaw, fc.Throttle, fc.ButtonFrontLeft, fc.ButtonFrontRight, fc.ButtonTopLeft, fc.ButtonTopRight, fc.ButtonBottomLeft, fc.ButtonBottomRight)
+		log.Printf("%4d, %4d, %4d, %4d, %t, %t, %t, %t, %t, %t", fc.Roll, fc.Pitch, fc.Yaw, fc.Throttle, fc.ButtonFrontLeft, fc.ButtonFrontRight, fc.ButtonTopLeft, fc.ButtonTopRight, fc.ButtonBottomLeft, fc.ButtonBottomRight)
 	}
 }
