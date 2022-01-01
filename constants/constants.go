7bfd26629aa74e653771387917e78233ae5252a6
marksaravi <mark.saravi@gmail.com>
2022-01-01 23:56:13 +0000

    using mcp3008 digital value

--- constants/constants.go
+++ constants/constants.go
@@ -1,4 +1,6 @@
 package constants
 
+import "github.com/marksaravi/drone-go/hardware/mcp3008"
+
 const RADIO_PAYLOAD_SIZE uint8 = 8
-const JOYSTICK_RESOLUTION = 250
+const JOYSTICK_RESOLUTION = mcp3008.DIGITAL_MAX_VALUE
