925b803719772f0b1e84436bdd24218cb1f3b964
marksaravi <mark.saravi@gmail.com>
2022-01-04 05:13:17 +0000

    restoring radio

--- cmd/test-nrf24l01-transmitter/main.go
+++ cmd/test-nrf24l01-transmitter/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
-	"log"
-	"sync"
+	"fmt"
 	"time"
 
 	"github.com/marksaravi/drone-go/config"
@@ -13,54 +11,42 @@ import (
 	"github.com/marksaravi/drone-go/utils"
 )
 
-func process(ctx context.Context, wg *sync.WaitGroup, radio models.RadioLink) {
-	defer wg.Done()
-
-	wg.Add(1)
-	transmitInterval := time.Second / 50
-	lastTransmit := time.Now()
-	var id byte = 0
-	var running bool = true
-	for running {
-		select {
-		case <-ctx.Done():
-			running = false
-		default:
-			if time.Since(lastTransmit) >= transmitInterval {
-				lastTransmit = time.Now()
-				var payload models.Payload
-				payload[0] = id
-				id++
-				if id > 250 {
-					id = 0
-				}
-				if id%50 == 0 {
-					log.Println("50 sent")
-				}
-				err := radio.Transmit(payload)
-				if err != nil {
-					log.Println(err.Error())
-				}
-			}
-
-		}
-	}
-}
-
 func main() {
-	log.SetFlags(log.Lmicroseconds)
 	hardware.InitHost()
-	radioConfigs := config.ReadConfigs().RemoteControl.Radio
-	radioNRF204 := nrf204.NewRadio(
+	radioConfigs := config.ReadConfigs().FlightControl.Radio
+	radio := nrf204.NewRadio(
 		radioConfigs.SPI.BusNumber,
 		radioConfigs.SPI.ChipSelect,
 		radioConfigs.CE,
 		radioConfigs.RxTxAddress,
 		radioConfigs.PowerDBm,
 	)
-	var wg sync.WaitGroup
-	ctx, cancel := context.WithCancel(context.Background())
-	go process(ctx, &wg, radioNRF204)
-	utils.WaitToAbortByENTER(cancel)
-	wg.Wait()
+	radio.TransmitterOn()
+	var roll float32 = 0
+	var altitude float32 = 0
+	var motorsEngaged bool = false
+	var numSend int = 0
+	start := time.Now()
+	var id uint32 = 0
+	for range time.Tick(time.Millisecond * 20) {
+		flightCommands := models.FlightCommands{
+			Roll:     14,
+			Pitch:    15,
+			Yaw:      0,
+			Throttle: 17,
+		}
+		id++
+		err := radio.Transmit(utils.SerializeFlightCommand(flightCommands))
+		if err != nil {
+			fmt.Println(err)
+		}
+		roll += 0.3
+		altitude += 1.34
+		motorsEngaged = !motorsEngaged
+		numSend++
+		if time.Since(start) >= time.Second {
+			start = time.Now()
+			fmt.Println("send ", numSend, " (", flightCommands, ")")
+		}
+	}
 }
