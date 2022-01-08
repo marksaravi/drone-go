02a474a2bfc60e63ff4b6944f605f73a290284b9
marksaravi <mark.saravi@gmail.com>
2022-01-08 14:34:53 +0000

    needs to work

--- pid/axiscontrol.go
+++ pid/axiscontrol.go
@@ -24,8 +24,8 @@ func (ac *axisControl) getP(input, gain float64) float64 {
 	return input * gain
 }
 
-func (ac *axisControl) getI(input, gain float64) float64 {
-	ac.iMemory = limitToMax(input*gain+ac.iMemory, ac.iMemoryLimit)
+func (ac *axisControl) getI(input float64, gain float64, dt time.Duration) float64 {
+	ac.iMemory = limitToMax(input*gain*float64(dt)/1000000000+ac.iMemory, ac.iMemoryLimit)
 
 	return ac.iMemory
 }
@@ -39,7 +39,7 @@ func (ac *axisControl) getD(input, gain float64, dt time.Duration) float64 {
 
 func (ac *axisControl) calc(input float64, dt time.Duration, gains *gains) float64 {
 	p := ac.getP(input, gains.P)
-	i := ac.getI(input, gains.I)
+	i := ac.getI(input, gains.I, dt)
 	d := ac.getD(input, gains.D, dt)
 	sum := p + i + d
 	return sum
