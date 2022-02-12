c1af8117055f7e149c18470c64852c15c8edfaa7
marksaravi <mark.saravi@gmail.com>
2022-02-12 01:10:12 +0000

    adding addresses and default values

---
+++ hardware/nrf204/enhanced-burst.go
@@ -0,0 +1,29 @@
+package nrf204
+
+func (tr *nrf204l01) ebReadRegister() {
+
+}
+
+func (tr *nrf204l01) ebWriteRegister() {
+
+}
+
+func (tr *nrf204l01) ebReadRxPayload() {
+
+}
+
+func (tr *nrf204l01) ebWriteTxPayload() {
+
+}
+
+func (tr *nrf204l01) ebFlushRx() {
+
+}
+
+func (tr *nrf204l01) ebFlushTx() {
+
+}
+
+func (tr *nrf204l01) ebReadStatusRegister() {
+
+}
--- hardware/nrf204/nrf204.go
+++ hardware/nrf204/nrf204.go
@@ -14,8 +14,53 @@ import (
 )
 
 const (
-	R_REGISTER byte = 0x1F
-	W_REGISTER byte = 0x20
+	R_REGISTER      byte = 0x1F
+	W_REGISTER      byte = 0x20
+	R_REGISTER_MASK byte = 0x0
+	W_REGISTER_MASK byte = 0x20
+)
+const (
+	ADDRESS_CONFIG      byte = 0x0
+	ADDRESS_EN_AA       byte = 0x1
+	ADDRESS_RXADDR      byte = 0x2
+	ADDRESS_SETUP_AW    byte = 0x3
+	ADDRESS_SETUP_RETR  byte = 0x4
+	ADDRESS_RF_CH       byte = 0x5
+	ADDRESS_RF_SETUP    byte = 0x6
+	ADDRESS_STATUS      byte = 0x7
+	ADDRESS_OBSERVE_TX  byte = 0x8
+	ADDRESS_CD          byte = 0x9
+	ADDRESS_RX_ADDR_P0  byte = 0xA
+	ADDRESS_RX_ADDR_P1  byte = 0xB
+	ADDRESS_RX_ADDR_P2  byte = 0xC
+	ADDRESS_RX_ADDR_P3  byte = 0xD
+	ADDRESS_RX_ADDR_P4  byte = 0xE
+	ADDRESS_RX_ADDR_P5  byte = 0xF
+	ADDRESS_TX_ADDR     byte = 0x10
+	ADDRESS_RX_PW_P0    byte = 0x11
+	ADDRESS_RX_PW_P1    byte = 0x12
+	ADDRESS_RX_PW_P2    byte = 0x13
+	ADDRESS_RX_PW_P3    byte = 0x14
+	ADDRESS_RX_PW_P4    byte = 0x15
+	ADDRESS_RX_PW_P5    byte = 0x16
+	ADDRESS_FIFO_STATUS byte = 0x17
+)
+
+const (
+	DEFAULT_CONFIG     byte = 0b00001000
+	DEFAULT_EN_AA      byte = 0b00111111
+	DEFAULT_EN_RXADDR  byte = 0b00000011
+	DEFAULT_SETUP_AW   byte = 0b00000011
+	DEFAULT_SETUP_RETR byte = 0b00000011
+	DEFAULT_RF_CH      byte = 0b00000010
+	DEFAULT_RF_SETUP   byte = 0b00001111
+	DEFAULT_STATUS     byte = 0b00001110
+	DEFAULT_RX_PW_P0   byte = 0b00000000
+	DEFAULT_RX_PW_P1   byte = 0b00000000
+	DEFAULT_RX_PW_P2   byte = 0b00000000
+	DEFAULT_RX_PW_P3   byte = 0b00000000
+	DEFAULT_RX_PW_P4   byte = 0b00000000
+	DEFAULT_RX_PW_P5   byte = 0b00000000
 )
 
 const (
@@ -76,6 +121,16 @@ type nrf204l01 struct {
 	conn       spi.Conn
 	powerDBm   byte
 	isReceiver bool
+}
+
+var configRegisters [8]byte = [8]byte{
+	DEFAULT_CONFIG,
+	DEFAULT_EN_AA,
+	DEFAULT_EN_RXADDR,
+	DEFAULT_SETUP_AW,
+	DEFAULT_SETUP_RETR,
+	DEFAULT_RF_CH,
+	DEFAULT_RF_SETUP,
 }
 
 func NewNRF204(
