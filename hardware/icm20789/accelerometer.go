cb6a229b978465c5a09c26dee86796a9551aa95b
marksaravi <mark.saravi@gmail.com>
2023-01-22 08:21:30 +0000

    new config file

--- hardware/icm20789/accelerometer.go
+++ hardware/icm20789/accelerometer.go
@@ -1,20 +1,5 @@
 package icm20789
 
-// func accelerometerFullScale(fsr string) float64 {
-// 	switch fsr {
-// 	case "2g":
-// 		return ACCEL_FULL_SCALE_2G
-// 	case "4g":
-// 		return ACCEL_FULL_SCALE_4G
-// 	case "8g":
-// 		return ACCEL_FULL_SCALE_8G
-// 	case "16g":
-// 		return ACCEL_FULL_SCALE_16G
-// 	default:
-// 		return ACCEL_FULL_SCALE_2G
-// 	}
-// }
-
 func (imu *imuICM20789) setupAccelerometer(fullScaleMask byte) {
 	accelsetup1, _ := imu.readByteFromRegister(ACCEL_CONFIG)
 	imu.writeRegister(ACCEL_CONFIG, accelsetup1|fullScaleMask)
