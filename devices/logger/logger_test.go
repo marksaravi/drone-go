af20c0d3231f9413692315b587c29b4f908d351e
marksaravi <mark.saravi@gmail.com>
2023-01-23 19:30:04 +0000

    waitgroup

--- devices/logger/logger_test.go
+++ devices/logger/logger_test.go
@@ -2,12 +2,14 @@ package logger
 
 import (
 	"encoding/binary"
+	"sync"
 	"testing"
 )
 
 func TestSerialiseFloat64(t *testing.T) {
 	const WANT float64 = 173.9
-	l := NewUDPLogger(0, 0)
+	var wg sync.WaitGroup
+	l := NewUDPLogger(&wg, 0, 0)
 	l.serialiseFloat64(WANT)
 	var i int16
 	binary.Read(l.buffer, binary.LittleEndian, &i)
