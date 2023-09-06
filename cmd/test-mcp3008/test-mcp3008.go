130224c20b6e0ebf0e9a757b48be15600d7f0530
marksaravi <mark.saravi@gmail.com>
2023-09-06 05:46:49 +0100

    mcp3008

---
+++ cmd/test-mcp3008/test-mcp3008.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/marksaravi/drone-go/hardware"
+	"github.com/marksaravi/drone-go/hardware/mcp3008"
+)
+
+func main() {
+	analogToDigitalSPIConn := hardware.NewSPIConnection(
+		0,
+		0,
+	)
+	xAxisAnalogToDigitalConvertor := mcp3008.NewMCP3008(
+		analogToDigitalSPIConn,
+		0,
+	)
+	// yAxisAnalogToDigitalConvertor := mcp3008.NewMCP3008(
+	// 	analogToDigitalSPIConn,
+	// 	1,
+	// )
+	// zAxisAnalogToDigitalConvertor := mcp3008.NewMCP3008(
+	// 	analogToDigitalSPIConn,
+	// 	2,
+	// )
+	// throttleAlogToDigitalConvertor := mcp3008.NewMCP3008(
+	// 	analogToDigitalSPIConn,
+	// 	3,
+	// )
+
+	for {
+		time.Sleep(time.Second / 2)
+		fmt.Println(xAxisAnalogToDigitalConvertor.Read())
+		// fmt.Println(yAxisAnalogToDigitalConvertor.Read())
+		// fmt.Println(zAxisAnalogToDigitalConvertor.Read())
+		// fmt.Println(throttleAlogToDigitalConvertor.Read())
+	}
+}
