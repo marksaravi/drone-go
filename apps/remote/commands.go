b93bec944b5182f7f60df363519c2bc97982ea57
marksaravi <mark.saravi@gmail.com>
2023-09-19 22:06:18 +0100

    transmitter

--- apps/remote/commands.go
+++ apps/remote/commands.go
@@ -2,10 +2,19 @@ package remote
 
 import "time"
 
+var roll byte = 0
+var pitch byte = 10
+var yaw byte = 20
+var throttle byte = 30
+
 func (r *remoteControl) ReadCommands() (commands, bool) {
 	if time.Since(r.lastCommand) < time.Second/time.Duration(r.commandPerSecond) {
 		return commands{}, false
 	}
 	r.lastCommand = time.Now()
-	return commands{}, true
+	roll++
+	pitch++
+	yaw++
+	throttle++
+	return commands{roll: roll, pitch: pitch, yaw: yaw, throttle: throttle}, true
 }
