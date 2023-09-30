e8b5c2d4420d81b934ba16681db458f0825a572e
marksaravi <mark.saravi@gmail.com>
2023-09-30 02:22:02 +0100

    imu configs

--- apps/drone/config.go
+++ apps/drone/config.go
@@ -9,12 +9,32 @@ import (
 	"github.com/marksaravi/drone-go/hardware"
 )
 
+type InertialDeviceConfigs struct {
+	FullScale string `json:"full_scale"`
+	Offsets   struct {
+		X uint16 `json:"x"`
+		Y uint16 `json:"y"`
+		Z uint16 `json:"z"`
+	} `json:"offsets"`
+}
+
 type DroneConfigs struct {
-	CommandsPerSecond int `json:"commands-per-second"`
-	Radio             struct {
-		RxTxAddress string                  `json:"rx-tx-address"`
-		SPI         hardware.SPIConnConfigs `json:"spi"`
-	} `json:"radio"`
+	IMU struct {
+		DataPerSecond     int     `json:"data-per-second"`
+		FilterCoefficient float64 `json:"filter-coefficient"`
+		MEMS              struct {
+			Accelerometer InertialDeviceConfigs   `json:"accelerometer"`
+			Gyroscope     InertialDeviceConfigs   `json:"gyroscope"`
+			SPI           hardware.SPIConnConfigs `json:"spi"`
+		} `json:"icm20789"`
+	} `json:"imu"`
+	RemoteControl struct {
+		CommandsPerSecond int `json:"commands-per-second"`
+		Radio             struct {
+			RxTxAddress string                  `json:"rx-tx-address"`
+			SPI         hardware.SPIConnConfigs `json:"spi"`
+		} `json:"radio"`
+	} `json:"remote-control"`
 }
 
 func ReadConfigs(configPath string) DroneConfigs {
--- cmd/drone/main.go
+++ cmd/drone/main.go
@@ -20,47 +20,43 @@ func main() {
 	configs := dronePackage.ReadConfigs("./configs/drone-configs.json")
 	log.Println(configs)
 
-	radioConfigs := configs.Radio
+	imuConfigs := configs.IMU
+	mems := icm20789.NewICM20789(icm20789.Configs{
+		Accelerometer: icm20789.InertialDeviceConfigs{
+			FullScale: imuConfigs.MEMS.Accelerometer.FullScale,
+			Offsets: icm20789.Offsets{
+				X: imuConfigs.MEMS.Accelerometer.Offsets.X,
+				Y: imuConfigs.MEMS.Accelerometer.Offsets.Y,
+				Z: imuConfigs.MEMS.Accelerometer.Offsets.Z,
+			},
+		},
+		Gyroscope: icm20789.InertialDeviceConfigs{
+			FullScale: imuConfigs.MEMS.Gyroscope.FullScale,
+			Offsets: icm20789.Offsets{
+				X: imuConfigs.MEMS.Gyroscope.Offsets.X,
+				Y: imuConfigs.MEMS.Gyroscope.Offsets.Y,
+				Z: imuConfigs.MEMS.Gyroscope.Offsets.Z,
+			},
+		},
+		SPI: imuConfigs.MEMS.SPI,
+	})
+	imudev := imu.NewIMU(mems, imu.Configs{
+		FilterCoefficient: imuConfigs.FilterCoefficient,
+	})
+
+	radioConfigs := configs.RemoteControl.Radio
 	radioLink := nrf24l01.NewNRF24L01EnhancedBurst(
 		radioConfigs.SPI,
 		radioConfigs.RxTxAddress,
 	)
 	radioReceiver := radio.NewRadioReceiver(radioLink)
-	icm20789Configs := icm20789.Configs{
-		Accelerometer: icm20789.InertialDeviceConfigs{
-			FullScale: "4g",
-			Offsets: icm20789.Offsets{
-				X: 32696,
-				Y: 836,
-				Z: 31979,
-			},
-		},
-		Gyroscope: icm20789.InertialDeviceConfigs{
-			FullScale: "500dps",
-			Offsets: icm20789.Offsets{
-				X: 0,
-				Y: 0,
-				Z: 0,
-			},
-		},
-		SPI: hardware.SPIConnConfigs{
-			BusNumber:  0,
-			ChipSelect: 0,
-		},
-	}
 
-	mems := icm20789.NewICM20789(icm20789Configs)
-	imuConfigs := imu.Configs{
-		FilterCoefficient: 0.001,
-	}
-
-	imudev := imu.NewIMU(mems, imuConfigs)
 	ctx, cancel := context.WithCancel(context.Background())
 	drone := dronePackage.NewDrone(dronePackage.DroneSettings{
-		ImuDataPerSecond:  1000,
+		ImuDataPerSecond:  imuConfigs.DataPerSecond,
 		Imu:               imudev,
 		Receiver:          radioReceiver,
-		CommandsPerSecond: configs.CommandsPerSecond,
+		CommandsPerSecond: configs.RemoteControl.CommandsPerSecond,
 	})
 
 	go func() {
