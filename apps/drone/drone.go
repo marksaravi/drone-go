cef46e70ebc82d5142ef267cab804cf05c1eb616
marksaravi <mark.saravi@gmail.com>
2023-09-30 07:56:53 +0100

    plotter buffering

--- apps/drone/drone.go
+++ apps/drone/drone.go
@@ -8,6 +8,9 @@ import (
 	"github.com/marksaravi/drone-go/devices/imu"
 )
 
+const PLOTTER_BUFFER_SIZE = 9216
+const PLOTTER_SAMPLE_RATE = 50
+
 type radioReceiver interface {
 	On()
 	Receive() ([]byte, bool)
@@ -26,30 +29,41 @@ type DroneSettings struct {
 }
 
 type droneApp struct {
-	imuDataPerSecond  int
+	imuDataPerSecond int
+	imu              Imu
+
+	rotations     imu.Rotations
+	accRotations  imu.Rotations
+	gyroRotations imu.Rotations
+
 	commandsPerSecond int
-	rotations         imu.Rotations
-	accRotations      imu.Rotations
-	gyroRotations     imu.Rotations
-	imu               Imu
 	receiver          radioReceiver
 	lastImuData       time.Time
 	lastCommand       time.Time
 	plotterActive     bool
+
+	plotterBuffer               []byte
+	plotterDataCounter          int
+	plotterSampleInterval       int
+	ploterSampleIntervalCounter int
 }
 
 func NewDrone(settings DroneSettings) *droneApp {
 	return &droneApp{
-		imu:               settings.Imu,
-		imuDataPerSecond:  settings.ImuDataPerSecond,
-		receiver:          settings.Receiver,
-		commandsPerSecond: settings.CommandsPerSecond,
-		lastCommand:       time.Now(),
-		lastImuData:       time.Now(),
-		plotterActive:     settings.PlotterActive,
-		rotations:         imu.Rotations{Roll: 0, Pitch: 0, Yaw: 0},
-		accRotations:      imu.Rotations{Roll: 0, Pitch: 0, Yaw: 0},
-		gyroRotations:     imu.Rotations{Roll: 0, Pitch: 0, Yaw: 0},
+		imu:                         settings.Imu,
+		imuDataPerSecond:            settings.ImuDataPerSecond,
+		receiver:                    settings.Receiver,
+		commandsPerSecond:           settings.CommandsPerSecond,
+		lastCommand:                 time.Now(),
+		lastImuData:                 time.Now(),
+		plotterActive:               settings.PlotterActive,
+		rotations:                   imu.Rotations{Roll: 0, Pitch: 0, Yaw: 0},
+		accRotations:                imu.Rotations{Roll: 0, Pitch: 0, Yaw: 0},
+		gyroRotations:               imu.Rotations{Roll: 0, Pitch: 0, Yaw: 0},
+		plotterBuffer:               make([]byte, 0, PLOTTER_BUFFER_SIZE),
+		plotterDataCounter:          0,
+		plotterSampleInterval:       settings.ImuDataPerSecond / PLOTTER_SAMPLE_RATE,
+		ploterSampleIntervalCounter: 0,
 	}
 }
 
@@ -63,7 +77,8 @@ func (d *droneApp) Start(ctx context.Context) {
 			imuok := d.ReadIMU()
 			command, commandok := d.ReceiveCommand()
 			if imuok {
-				d.PlotterData()
+				d.BufferPlotterData()
+				d.SendPlotterData()
 			}
 			if (commandok) && time.Since(lp) > time.Second/10 {
 				lp = time.Now()
--- apps/drone/readings.go
+++ apps/drone/readings.go
@@ -1,9 +1,9 @@
 package drone
 
 import (
+	"encoding/binary"
+	"fmt"
 	"time"
-
-	"github.com/marksaravi/drone-go/devices/imu"
 )
 
 func (d *droneApp) ReadIMU() bool {
@@ -29,11 +29,52 @@ func (d *droneApp) ReceiveCommand() ([]byte, bool) {
 	return d.receiver.Receive()
 }
 
-func (d *droneApp) PlotterData() {
+func (d *droneApp) SendPlotterData() {
+	d.plotterDataCounter++
+	if d.plotterDataCounter < PLOTTER_SAMPLE_RATE {
+		return
+	}
+	d.plotterDataCounter = 0
+	buffer := make([]byte, len(d.plotterBuffer))
+	copy(buffer, d.plotterBuffer)
+	d.plotterBuffer = make([]byte, 0, PLOTTER_BUFFER_SIZE)
+	go func(b []byte) {
+		fmt.Println("sending plotter data")
+	}(buffer)
+}
+func (d *droneApp) BufferPlotterData() {
 	if !d.plotterActive {
 		return
 	}
+	d.ploterSampleIntervalCounter++
+	if d.ploterSampleIntervalCounter < d.plotterSampleInterval {
+		return
+	}
+	d.ploterSampleIntervalCounter = 0
+	d.plotterBuffer = append(d.plotterBuffer, d.SerializeRotations()...)
 }
 
-func SerializeRotations(r imu.Rotations) {
+func (d *droneApp) SerializeRotations() []byte {
+	data := make([]byte, 0, 64)
+	t := make([]byte, 8)
+	binary.LittleEndian.PutUint64(t, uint64(time.Now().UnixMicro()))
+	data = append(data, t...)
+	data = append(data, rotationToInt(d.rotations.Roll)...)
+	data = append(data, rotationToInt(d.rotations.Pitch)...)
+	data = append(data, rotationToInt(d.rotations.Yaw)...)
+	data = append(data, rotationToInt(d.accRotations.Roll)...)
+	data = append(data, rotationToInt(d.accRotations.Pitch)...)
+	data = append(data, rotationToInt(d.accRotations.Yaw)...)
+	data = append(data, rotationToInt(d.gyroRotations.Roll)...)
+	data = append(data, rotationToInt(d.gyroRotations.Pitch)...)
+	data = append(data, rotationToInt(d.gyroRotations.Yaw)...)
+
+	return data
+}
+
+func rotationToInt(r float64) []byte {
+	n := uint16(r*10) + 16000
+	d := []byte{0, 0}
+	binary.LittleEndian.PutUint16(d, n)
+	return d
 }
