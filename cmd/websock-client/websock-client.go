e5f5338f043895065cf9271aedc1f7d5abb275c0
marksaravi <mark.saravi@gmail.com>
2023-10-05 07:52:55 +1100

    geacefully stop http server

---
+++ cmd/websock-client/websock-client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/net/websocket"
+)
+
+func main() {
+	origin := "http://localhost/"
+	url := "ws://localhost:12345/ws"
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
+		log.Fatal(err)
+	}
+	var msg = make([]byte, 512)
+	var n int
+	if n, err = ws.Read(msg); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Received: %s.\n", msg[:n])
+}
---
+++ cmd/websock-server/websock-server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	log.SetFlags(0)
+
+	var listenAddress = flag.String("listen", ":8000", "Listen address.")
+
+	flag.Parse()
+
+	if flag.NArg() != 0 {
+		flag.Usage()
+		log.Fatalf("\nERROR You MUST NOT pass any positional arguments")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("HTTP %s %s%s\n", r.Method, r.Host, r.URL)
+
+		if r.URL.Path != "/" {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(time.Now().Local().Format(time.RFC1123Z)))
+	})
+
+	log.Printf("Listening at http://%s", *listenAddress)
+
+	httpServer := http.Server{
+		Addr: *listenAddress,
+	}
+
+	idleConnectionsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+		if err := httpServer.Shutdown(context.Background()); err != nil {
+			log.Printf("HTTP Server Shutdown Error: %v", err)
+		}
+		close(idleConnectionsClosed)
+	}()
+
+	if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe Error: %v", err)
+	}
+
+	<-idleConnectionsClosed
+
+	log.Printf("Bye bye")
+}
