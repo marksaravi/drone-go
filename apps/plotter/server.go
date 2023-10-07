0b64c264d5d794dc3366c3bb241889f3d7a9af70
marksaravi <mark.saravi@gmail.com>
2023-10-08 10:52:50 +1100

    plotter start and stop

--- apps/plotter/server.go
+++ apps/plotter/server.go
@@ -19,28 +19,44 @@ type PlotterSettings struct {
 }
 
 func NewPlotter(settings PlotterSettings) *plotter {
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+
 	return &plotter{
-		sigint: make(chan os.Signal, 1),
+		sigint: sigint,
 	}
 }
 
 func (p *plotter) StartPlotter() {
+	log.SetFlags(log.Lmicroseconds)
 	ctx, cancel := context.WithCancelCause(context.Background())
 	var wg sync.WaitGroup
-	p.waitForInterrupt(cancel, &wg)
-
+	p.waitForInterrupt(ctx, cancel, &wg)
+	p.stopByEnter(cancel)
+	log.Println("Plotter started...")
 	wg.Wait()
-	fmt.Printf("plotter stopped for: %v", context.Cause(ctx))
+	log.Printf("plotter stopped for: %v\n", context.Cause(ctx))
 }
 
-func (p *plotter) waitForInterrupt(cancel context.CancelCauseFunc, wg *sync.WaitGroup) {
+func (p *plotter) waitForInterrupt(ctx context.Context, cancel context.CancelCauseFunc, wg *sync.WaitGroup) {
 	wg.Add(1)
 	go func() {
+		defer log.Printf("Stopping Plotter...")
+		defer close(p.sigint)
 		defer wg.Done()
-		signal.Notify(p.sigint, os.Interrupt)
-		<-p.sigint
-		log.Printf("Stopping Plotter...")
-		cancel(fmt.Errorf("signal interrupt"))
-		close(p.sigint)
+
+		select {
+		case <-p.sigint:
+			cancel(fmt.Errorf("signal interrupt"))
+		case <-ctx.Done():
+			return
+		}
+	}()
+}
+
+func (p *plotter) stopByEnter(cancel context.CancelCauseFunc) {
+	go func() {
+		fmt.Scanln()
+		cancel(fmt.Errorf("enter"))
 	}()
 }
