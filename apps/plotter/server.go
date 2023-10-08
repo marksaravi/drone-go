4cabc58868f90dce64d60fd48912931e0478945c
marksaravi <mark.saravi@gmail.com>
2023-10-08 18:57:54 +1100

    udp packets

--- apps/plotter/server.go
+++ apps/plotter/server.go
@@ -4,24 +4,33 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
+	"time"
 
 	"nhooyr.io/websocket"
 )
+
+const UDP_BUFFER_SIZE = 8192
 
 type plotter struct {
 	websocketConn     *websocket.Conn
 	httpServerAddress string
 	httpServer        *http.Server
+	udpServerAddress  string
+	udpConn           *net.UDPConn
+	udpBuffer         []byte
 	stopPlotter       context.CancelCauseFunc
 }
 
 func NewPlotter() *plotter {
 	return &plotter{
 		httpServerAddress: "localhost:3000",
+		udpServerAddress:  "localhost:8000",
 	}
 }
 
@@ -31,13 +40,26 @@ func (p *plotter) StartPlotter() {
 	ctx, p.stopPlotter = context.WithCancelCause(context.Background())
 	var wg sync.WaitGroup
 
+	p.createUdpServer()
 	p.createHttpServer()
 	p.waitForInterrupt(ctx, &wg)
 	p.stopByEnter()
-	p.startHttpServer(&wg)
+	// p.startHttpServer(&wg)
+	p.startUdpServer(&wg)
 	log.Println("Plotter started...")
 	wg.Wait()
 	log.Printf("plotter stopped for: %v\n", context.Cause(ctx))
+}
+
+func (p *plotter) createUdpServer() {
+	udpAddr, _ := net.ResolveUDPAddr("udp", p.udpServerAddress)
+	var err error
+	p.udpConn, err = net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		p.stopPlotter(err)
+		return
+	}
+	p.udpBuffer = make([]byte, UDP_BUFFER_SIZE)
 }
 
 func (p *plotter) createHttpServer() {
@@ -48,6 +70,30 @@ func (p *plotter) createHttpServer() {
 		Addr:    p.httpServerAddress,
 		Handler: mux,
 	}
+}
+
+func (p *plotter) startUdpServer(wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		log.Println("Starting UDP Server ...")
+		counter := 0
+		lastPrint := time.Now()
+		for {
+			nBytes, _, err := p.udpConn.ReadFromUDP(p.udpBuffer)
+			if err != nil && strings.Contains(err.Error(), "closed network connection") {
+				return
+			}
+			if err == nil && nBytes > 0 {
+				counter++
+				if time.Since(lastPrint) >= time.Second {
+					log.Printf("udp packet size: %d, packet per second: %d\n", nBytes, counter)
+					counter = 0
+					lastPrint = time.Now()
+				}
+			}
+		}
+	}()
 }
 
 func (p *plotter) startHttpServer(wg *sync.WaitGroup) {
@@ -70,9 +116,11 @@ func (p *plotter) waitForInterrupt(ctx context.Context, wg *sync.WaitGroup) {
 	go func() {
 		sigint := make(chan os.Signal, 1)
 		signal.Notify(sigint, os.Interrupt)
-		defer log.Printf("Stopping Plotter...")
-		defer p.shutdownHttpServer(wg)
+		defer p.udpConn.Close()
+		log.Println("Stopping UDP Server ...")
+		// defer p.shutdownHttpServer(wg)
 		defer close(sigint)
+		defer log.Printf("Stopping Plotter...")
 
 		select {
 		case <-sigint:
