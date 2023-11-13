646165c3e1c2d860eb684ef320fd07674a568cbd
Mark Saravi <mark.saravi@gmail.com>
2023-11-13 13:22:03 +1100

    Imu calibration (#115)
    accelerometer configs

--- hardware/mems/icm20789/icm20789.go
+++ hardware/mems/icm20789/icm20789.go
@@ -9,17 +9,46 @@ import (
 )
 
 const (
-	ACCEL_XOUT_H byte = 0x3B
-	ACCEL_CONFIG byte = 0x1C
-	GYRO_CONFIG  byte = 0x1B
-	WHO_AM_I     byte = 0x75
-	PWR_MGMT_1   byte = 0x6B
-	PWR_MGMT_2   byte = 0x6C
+	ADDRESS_ACCEL_XOUT_H byte = 0x3B
+	ADDRESS_ACCEL_CONFIG byte = 0x1C
+	ADRESS_GYRO_CONFIG   byte = 0x1B
+	ADDRESS_PWR_MGMT_1   byte = 0x6B
+	ADDRESS_PWR_MGMT_2   byte = 0x6C
+	ADDRESS_WHO_AM_I     byte = 0x75
+	ADDRESS_XA_OFFSH     byte = 0x77
+	ADDRESS_XA_OFFSL     byte = 0x78
+	ADDRESS_YA_OFFSH     byte = 0x7A
+	ADDRESS_YA_OFFSL     byte = 0x7B
+	ADDRESS_ZA_OFFSH     byte = 0x7D
+	ADDRESS_ZA_OFFSL     byte = 0x7E
 )
 
 const (
-	PWR_MGMT_1_CONFIG byte = 0b00000000
-	PWR_MGMT_2_CONFIG byte = 0b00000000
+	PWR_MGMT_1_CONFIG_DEVICE_RESET byte = 0b10000000
+	PWR_MGMT_1_CONFIG              byte = 0b00000000
+	PWR_MGMT_2_CONFIG              byte = 0b00000000
+)
+
+const (
+	ACCEL_CONFIG_DISABLE_SELF_TESTS  byte = 0b00000000
+	ACCEL_CONFIG_2G                  byte = 0b00000000
+	ACCEL_CONFIG_4G                  byte = 0b00001000
+	ACCEL_CONFIG_8G                  byte = 0b00010000
+	ACCEL_CONFIG_16G                 byte = 0b00011000
+	ACCEL_CONFIG2_FIFO_SIZE_512      byte = 0b00000000
+	ACCEL_CONFIG2_DEC2_CFG_4_SAMPLE  byte = 0b00000000
+	ACCEL_CONFIG2_DEC2_CFG_8_SAMPLE  byte = 0b00010000
+	ACCEL_CONFIG2_DEC2_CFG_16_SAMPLE byte = 0b00100000
+	ACCEL_CONFIG2_DEC2_CFG_32_SAMPLE byte = 0b00110000
+	ACCEL_CONFIG2_ACCEL_FCHOICE_B    byte = 0b00001000 //3-dB BW (Hz) 1046.0
+	ACCEL_CONFIG2_A_DLPF_CFG_3dB_BW0 byte = 0b00000000 //3-dB BW (Hz) 218.1
+	ACCEL_CONFIG2_A_DLPF_CFG_3dB_BW1 byte = 0b00000001 //3-dB BW (Hz) 218.1
+	ACCEL_CONFIG2_A_DLPF_CFG_3dB_BW2 byte = 0b00000010 //3-dB BW (Hz) 99.0
+	ACCEL_CONFIG2_A_DLPF_CFG_3dB_BW3 byte = 0b00000011 //3-dB BW (Hz) 44.8
+	ACCEL_CONFIG2_A_DLPF_CFG_3dB_BW4 byte = 0b00000100 //3-dB BW (Hz) 21.2
+	ACCEL_CONFIG2_A_DLPF_CFG_3dB_BW5 byte = 0b00000101 //3-dB BW (Hz) 10.2
+	ACCEL_CONFIG2_A_DLPF_CFG_3dB_BW6 byte = 0b00000110 //3-dB BW (Hz) 5.1
+	ACCEL_CONFIG2_A_DLPF_CFG_3dB_BW7 byte = 0b00000111 //3-dB BW (Hz) 420.0
 )
 
 const (
@@ -76,8 +105,13 @@ func NewICM20789(configs Configs) *memsIcm20789 {
 	return &m
 }
 
+func (m *memsIcm20789) WhoAmI() (byte, error) {
+	memsData, err := m.readRegister(ADDRESS_WHO_AM_I, 1)
+	return memsData[0], err
+}
+
 func (m *memsIcm20789) Read() (mems.Mems6DOFData, error) {
-	memsData, err := m.readRegister(ACCEL_XOUT_H, RAW_DATA_SIZE)
+	memsData, err := m.readRegister(ADDRESS_ACCEL_XOUT_H, RAW_DATA_SIZE)
 	if err != nil {
 		return mems.Mems6DOFData{}, err
 	}
@@ -111,10 +145,10 @@ func (m *memsIcm20789) writeRegister(address byte, data ...byte) error {
 }
 
 func (m *memsIcm20789) setupPower() {
-	m.writeRegister(PWR_MGMT_1, 0x80) // soft reset
-	delay(1)
-	m.writeRegister(PWR_MGMT_1, PWR_MGMT_1_CONFIG)
-	delay(1)
+	m.writeRegister(ADDRESS_PWR_MGMT_1, PWR_MGMT_1_CONFIG_DEVICE_RESET)
+	delay(10)
+	m.writeRegister(ADDRESS_PWR_MGMT_1, PWR_MGMT_1_CONFIG)
+	delay(10)
 }
 
 func (m *memsIcm20789) memsDataToAccelerometer(memsData []byte) mems.XYZ {
