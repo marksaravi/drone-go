7c1468b763e01188451aed63303c853ea9ba7947
marksaravi <mark.saravi@gmail.com>
2024-07-09 01:10:02 +0100

    percision

--- utils/kalman-filter.go
+++ utils/kalman-filter.go
@@ -1,18 +1,26 @@
 package utils
 
+import (
+	"math"
+)
+
 type kalmanFilter struct {
-	prevValue float64
-	gain      float64
+	prevValue        float64
+	decimalPrecision float64
+	gain             float64
 }
 
-func NewKalmanFilter(gain float64) *kalmanFilter {
+func NewKalmanFilter(gain float64, decimalPrecision int) *kalmanFilter {
 	return &kalmanFilter{
-		prevValue: 0,
-		gain:      gain,
+		prevValue:        0,
+		gain:             gain,
+		decimalPrecision: math.Pow10(decimalPrecision),
 	}
 }
 
 func (k *kalmanFilter) Value(value float64) float64 {
+
+	value = math.Round(value*k.decimalPrecision) / k.decimalPrecision
 	smoothValue := k.prevValue*(1-k.gain) + value*k.gain
 	k.prevValue = smoothValue
 	return smoothValue
