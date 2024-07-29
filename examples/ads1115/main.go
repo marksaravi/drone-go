2ec40880d3caa17401090a64084a34898bf7fca2
marksaravi <mark.saravi@gmail.com>
2024-07-29 17:30:56 +0100

    read i2c

--- examples/ads1115/main.go
+++ examples/ads1115/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"periph.io/x/conn/v3/i2c"
+	"periph.io/x/conn/v3/i2c/i2creg"
+	"github.com/marksaravi/drone-go/hardware"
 	"github.com/marksaravi/drone-go/hardware/ads1115"
 )
 
 func main() {
-	atod := ads1115.NewADS1115();
+	hardware.HostInitialize()
+	b, _ := i2creg.Open("")
+	defer b.Close()
+	i2cdev := &i2c.Dev{Addr: 0x48, Bus: b}
+
+	atod := ads1115.NewADS1115(i2cdev);
 
 	for channel:=0; channel<4; channel++ {
 		voltage := atod.Read(0)
--- hardware/ads1115/ads1115.go
+++ hardware/ads1115/ads1115.go
@@ -1,15 +1,41 @@
 package ads1115
 
-type ads1115AtoD struct {
+import (
+	"periph.io/x/conn/v3/i2c"
+)
 
+const (
+	CONVERSION_REGISTER_ADDRESS = byte(0)
+	CONFIG_RREGISTER_ADDRESS =    byte(1)
+	LO_THRESH_REGISTER_ADDRESS =  byte(2)
+	HI_THRESH_REGISTER_ADDRESS =  byte(3)
+)
+
+type ads1115AtoD struct {
+	i2cDev  *i2c.Dev
 }
 
-func NewADS1115() *ads1115AtoD {
+func NewADS1115(i2cDev  *i2c.Dev) *ads1115AtoD {
 	return &ads1115AtoD {
-
+		i2cDev: i2cDev,
 	}
 }
 
 func (d *ads1115AtoD) Read(channel int) int {
-	return 0
+	b,_ := d.readConversion()
+	uv := uint16(b[0]) | uint16(b[1])<<8
+	return int(uv)
 }
+
+func (d *ads1115AtoD) readConversion() ([]byte, error) {
+	r := make([]byte, 2)
+	w := []byte{CONVERSION_REGISTER_ADDRESS}
+	err := d.i2cDev.Tx(w, r)
+	return r, err
+}
+
+// func (d *ads1115AtoD) writeByte(address byte) error {
+// 	write := []byte{offset, b}
+// 	_, err := d.i2cDev.Write(write)
+// 	return err
+// }
