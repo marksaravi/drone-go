d42e3ee3e878937748c6abdbb7803c693e252a15
marksaravi <mark.saravi@gmail.com>
2024-07-30 23:15:22 +0100

    methods

--- hardware/ads1115/ads1115.go
+++ hardware/ads1115/ads1115.go
@@ -26,12 +26,19 @@ const (
 	COMP_QUE =                    byte(0b00000011)
 )
 
+const ADS1X15_REG_POINTER_MASK =byte(0x03)      ///< Point mask
+const ADS1X15_REG_POINTER_CONVERT =byte(0x00)   ///< Conversion
+const ADS1X15_REG_POINTER_CONFIG =byte(0x01)    ///< Configuration
+const ADS1X15_REG_POINTER_LOWTHRESH =byte(0x02) ///< Low threshold
+const ADS1X15_REG_POINTER_HITHRESH =byte(0x03)  ///< High threshold
 
+const ADS1X15_REG_CONFIG_CQUE_1CONV = 0x0000
+const ADS1X15_REG_CONFIG_MODE_SINGLE = 0x0100
 const ADS1X15_REG_CONFIG_MUX_SINGLE_0 = 0x4000 ///< Single-ended AIN0
 const ADS1X15_REG_CONFIG_MUX_SINGLE_1 = 0x5000 ///< Single-ended AIN1
 const ADS1X15_REG_CONFIG_MUX_SINGLE_2 = 0x6000 ///< Single-ended AIN2
 const ADS1X15_REG_CONFIG_MUX_SINGLE_3 = 0x7000 ///< Single-ended AIN3
-
+const ADS1X15_REG_CONFIG_OS_SINGLE = 0x8000
 var MUX_BY_CHANNEL []uint16 = []uint16 {
     ADS1X15_REG_CONFIG_MUX_SINGLE_0, ///< Single-ended AIN0
     ADS1X15_REG_CONFIG_MUX_SINGLE_1, ///< Single-ended AIN1
@@ -124,14 +131,86 @@ func (d *ads1115AtoD) ReadADC_SingleEnded(channel byte) uint16 {
 	if (channel > 3) {
 	  return 0;
 	}
-	return 1
   
-	// startADCReading(MUX_BY_CHANNEL[channel], /*continuous=*/false);
+	d.startADCReading(MUX_BY_CHANNEL[channel]);
   
-	// // Wait for the conversion to complete
-	// while (!conversionComplete())
-	//   ;
+	// Wait for the conversion to complete
+	for !d.conversionComplete() {}
   
-	// // Read the conversion results
-	// return getLastConversionResults();
+	// Read the conversion results
+	return d.getLastConversionResults();
+}
+
+func (d *ads1115AtoD) startADCReading(mux uint16) {
+	// Start with default values
+	// uint16_t config =
+	// 	ADS1X15_REG_CONFIG_CQUE_1CONV |   // Set CQUE to any value other than
+	// 									  // None so we can use it in RDY mode
+	// 	ADS1X15_REG_CONFIG_CLAT_NONLAT |  // Non-latching (default val)
+	// 	ADS1X15_REG_CONFIG_CPOL_ACTVLOW | // Alert/Rdy active low   (default val)
+	// 	ADS1X15_REG_CONFIG_CMODE_TRAD;    // Traditional comparator (default val)
+	config := uint16(0)
+  
+	config |= ADS1X15_REG_CONFIG_MODE_SINGLE;
+  
+	// Set PGA/voltage range
+	config |= uint16(AMP_FS_2)
+  
+	// Set data rate
+	config |= uint16(DATA_RATE_128SPS)
+  
+	// Set channels
+	config |= mux
+  
+	// Set 'start single-conversion' bit
+	config |= ADS1X15_REG_CONFIG_OS_SINGLE
+  
+	// Write config register to the ADC
+	d.writeRegister(ADS1X15_REG_POINTER_CONFIG, config)
+  
+	// Set ALERT/RDY to RDY mode.
+	d.writeRegister(ADS1X15_REG_POINTER_HITHRESH, 0x8000)
+	d.writeRegister(ADS1X15_REG_POINTER_LOWTHRESH, 0x0000)
   }
+
+  func (d *ads1115AtoD) writeRegister(reg byte, value uint16) {
+
+  }
+
+//   void Adafruit_ADS1X15::writeRegister(uint8_t reg, uint16_t value) {
+// 	buffer[0] = reg;
+// 	buffer[1] = value >> 8;
+// 	buffer[2] = value & 0xFF;
+// 	m_i2c_dev->write(buffer, 3);
+//   }
+
+func (d *ads1115AtoD) conversionComplete() bool {
+	return (d.readRegister(ADS1X15_REG_POINTER_CONFIG) & 0x8000) != 0;
+}
+
+func (d *ads1115AtoD) readRegister(reg byte) uint16 {
+	// buffer[0] = reg;
+	// m_i2c_dev->write(buffer, 1);
+	// m_i2c_dev->read(buffer, 2);
+	// return ((buffer[0] << 8) | buffer[1]);
+	return 0;
+}
+
+func (d *ads1115AtoD) getLastConversionResults() uint16 {
+	return 0
+}
+// int16_t Adafruit_ADS1X15::getLastConversionResults()  {
+// 	// Read the conversion results
+// 	uint16_t res = readRegister(ADS1X15_REG_POINTER_CONVERT) >> m_bitShift;
+// 	if (m_bitShift == 0) {
+// 	  return (int16_t)res;
+// 	} else {
+// 	  // Shift 12-bit results right 4 bits for the ADS1015,
+// 	  // making sure we keep the sign bit intact
+// 	  if (res > 0x07FF) {
+// 		// negative number - extend the sign to 16th bit
+// 		res |= 0xF000;
+// 	  }
+// 	  return (int16_t)res;
+// 	}
+//   }
