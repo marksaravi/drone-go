57fdb53422d4162170a035729023edf1ed6b3909
marksaravi <mark.saravi@gmail.com>
2024-09-05 03:53:14 +0100

    simple

--- constants/constants.go
+++ constants/constants.go
@@ -3,6 +3,6 @@ package constants
 const RADIO_PAYLOAD_SIZE uint8 = 10
 const JOY_STICK_INPUT_RANGE = uint16(32768)
 
-const ESC_FREQUENCY = 500
+const ESC_FREQUENCY = 480
 const ESC_MIN_PW float64 = 0.001
-const ESC_MAX_PW float64 = 0.0019
+const ESC_MAX_PW float64 = 0.002
